2b7e272ec453420cb76894318e1a99713789cbc9
BrunoMmS <brunommsosa@gmail.com>
2025-04-15 08:22:22 -0300

    First Commit

---
+++ src/test/java/gpps_backend/GppsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gpps_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GppsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
