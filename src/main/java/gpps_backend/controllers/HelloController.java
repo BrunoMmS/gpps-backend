6258bb34e19b564f9ce4301cd6baf92d41bf2074
BrunoMmS <brunitosos0102@gmail.com>
2025-05-10 17:07:42 -0300

    Test de server inicializado

---
+++ src/main/java/gpps_backend/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package gpps_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hol, spring inicializado!";
+    }
+}
