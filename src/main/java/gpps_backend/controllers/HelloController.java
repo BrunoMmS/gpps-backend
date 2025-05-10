941ad1fee9bc1b52374a04ef9e2f07ca06cd1831
Joaco <negueluajoa@gmail.com>
2025-05-10 18:01:55 -0300

    prueba

--- src/main/java/gpps_backend/controllers/HelloController.java
+++ src/main/java/gpps_backend/controllers/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hol, spring inicializado!, actualizacion para joaco !!";
+        return "bueno che";
     }
 }
