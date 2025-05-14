05ba96e219a2b128934d3d090417e538fc9c9224
BrunoMmS <brunitosos0102@gmail.com>
2025-05-13 21:48:29 -0300

    Se corrigio un error, se agrego el login, y documentacion de como arrancar la app

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from routers.user_router import user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Register the user router
 app.include_router(user_router)
--- routers/user_router.py
+++ routers/user_router.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
 from typing import Dict
 
-from schema.user_schema import UserCreate
+from schema.user_schema import UserCreate, UserLogin
 
 # Mock database and utility functions
 users_db: Dict[str, Dict] = {}
@@ -16,14 +16,52 @@ def get_user(token: str):
 
 user_router = APIRouter()
 
-@user_router.post("/register", response_model=UserCreate)
+@user_router.post("/users/register", response_model=UserCreate)
 def register(user: UserCreate):
     if user.username in users_db:
         raise HTTPException(status_code=400, detail="Username already exists")
+
     hashed_password = get_password_hash(user.password)
-    users_db[user.username] = {"username": user.username, "hashed_password": hashed_password, "role": user.role}
-    return UserCreate(username=user.username, role=user.role)
+    users_db[user.username] = {
+        "id": len(users_db) + 1,
+        "username": user.username,
+        "lastname": user.lastname,
+        "email": user.email,
+        "role": user.role,
+        "hashed_password": hashed_password
+    }
+
+    return UserCreate(
+        id=len(users_db),
+        username=user.username,
+        lastname=user.lastname,
+        email=user.email,
+        role=user.role,
+        password=user.password
+    )
 
 @user_router.get("/users")
 def list_users():
     return [{"username": user["username"], "role": user["role"]} for user in users_db.values()]
+
+
+@user_router.post("/users/login")
+def login(credentials: UserLogin):
+    user = None
+    for u in users_db.values():
+        if u["email"] == credentials.email:
+            user = u
+            break
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Usuario no encontrado")
+
+    hashed_input_password = get_password_hash(credentials.password)
+    if user["hashed_password"] != hashed_input_password:
+        raise HTTPException(status_code=401, detail="Contraseña incorrecta")
+
+    return {
+        "username": user["username"],
+        "email": user["email"],
+        "role": user["role"]
+    }
